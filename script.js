8dff79f86874bb21106901a2cd90b799f351f4cb
jonjigoncalves <jonjigoncalves@gmail.com>
2023-07-04 12:58:53 -0400

    fixed issues with dissapearing fetch

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var searchbtn = document.querySelector('.btn');
+var formID = document.getElementById('formID');
 var apiKey = '5fb3261485fbd16f27a2aaa4d57b4602';
 var cities = JSON.parse(localStorage.getItem('cities')) || [];
 var cityInput = document.querySelector('.form-control');
 var weatherData = [];
+var currentWeatherEL = $('#currentWeather');
 
-
+// finished, and test
 function getGeo(city) {
-  console.log(city);
-  fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=' + apiKey)
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=' + apiKey)
     .then(function (res) {
       return res.json();
     })
@@ -19,16 +20,25 @@ function getGeo(city) {
     });
    
 }
-
+// finished and tested
 function getWeather(lat, lon) {
-  console.log(lat, lon);
-  fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + apiKey)
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + apiKey)
     .then(function (res) {
       return res.json();
     })
     .then(function (data) {
       console.log(data);
-      weatherData.push(data);
+         var weatherDiv = $('<div>').addClass('card');
+      var titleEL = $('<h3>').addClass('card-title').text(data.name);
+      var imageIcon = $('<img>').attr('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` )
+      var tempEL = $('<h4>').addClass('card-text').text('Temp: ' + Math.round(data.main.temp) + String.fromCharCode(186));
+      var humidtyEL = $('<h4>').addClass('card-text').text('Humidty: ' + data.main.humidity + '%' );
+      var windEL = $('<h4>').addClass('card-text').text('Wind Speed: ' + data.wind.speed + 'mph' );
+      
+      currentWeatherEL.append(weatherDiv.append(titleEL.append(imageIcon), tempEL, humidtyEL,windEL));
+      
+
+      // weatherData.push(data);
       getFiveDayForecast(lat, lon);
     });
 }
@@ -40,23 +50,33 @@ function getFiveDayForecast(lat, lon) {
       return res.json()
     })
     .then(function (data) {
-        for (var i = 0; i < data.list.length; i++) {
+      console.log(data);
+              for (var i = 0; i < data.list.length; i++) {
             var forecast = data.list[i];
+            
+            var targetTime = forecast.dt_txt.split(' ').pop();
+            console.log(targetTime);
+            if(targetTime === "12:00:00"){
+              
+              weatherData.push(forecast);
+            }
             
         
         }
+        console.log(weatherData);
     });
 }
 
-searchbtn.addEventListener('click', function(){
-    var city = cityInput.value;
-    if(city === '') {
-        return ;
-    }
-    if(!cities.includes(city)) {
-        cities.push(city);
-    }
-    localStorage.setItem('cities', JSON.stringify(cities));
-    console.log(cities);
+formID.addEventListener('submit', function(evt){
+  evt.preventDefault();
+   var city = cityInput.value;
+    // if(city === '') {
+    //     return ;
+    // }
+    // if(!cities.includes(city)) {
+    //     cities.push(city);
+    // }
+    // localStorage.setItem('cities', JSON.stringify(cities));
+    // console.log(cities);
     getGeo(city);
 });
